c156059c6f1a4444afdeada59bba29038c1a1a53
pranita <pranitagiri99@gmail.com>
2022-05-26 18:51:22 +0530

    Queen2

--- pranita-lib/src/main/java/pranita/Santhosh.java
+++ pranita-lib/src/main/java/pranita/Santhosh.java
@@ -5,6 +5,7 @@ public class Santhosh {
 	public static void main(String[] args) {
 		System.out.println("pranita is real queen ");
 		System.out.println("and very innocent girl ");
+		System.out.println("And our team is the best");
 		
 
 	}
